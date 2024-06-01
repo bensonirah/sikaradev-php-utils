2a680f2aeacf0a3d6b3d63d4feed0f2ff7e1fbc1
bensonirah <bensonirah@gmail.com>
2024-06-01 22:57:34 +0300

    Update zsh_history function

--- src/Console/ZshConsoleHistory.php
+++ src/Console/ZshConsoleHistory.php
@@ -15,17 +15,16 @@ final class ZshConsoleHistory extends AbstractConsoleHistory
     public function contents(): array
     {
         $console_history = file_get_contents($this->path());
-        return Collection::of(explode("\n", $console_history))
+        $console_history = preg_replace('/^:\s+/m', ':1;', $console_history);
+        return Collection::of(explode(":1;", $console_history))
             ->filter(fn(int $key, string $entry) => $entry != '')
             ->map(function (string $entry) {
-                [$eventTime, $command] = explode(';', $entry);
-                $eventTime = preg_replace('/^:\s+/', '', $eventTime);
-                [$runAt, $executionTime] = explode(':', $eventTime);
+                [$eventTime, $command] = explode(':0;', $entry);
                 $dateTime = new \DateTime();
-                $dateTime->setTimestamp((int)$runAt);
+                $dateTime->setTimestamp((int)$eventTime);
                 return [
-                    'command' => $command,
-                    'executionTime' => (int)$executionTime,
+                    'command' => preg_replace('/\n/', '', $command),
+                    'executionTime' => (int)$eventTime,
                     'dateTime' => $dateTime->format('Y-m-d H:i:s')
                 ];
             })
