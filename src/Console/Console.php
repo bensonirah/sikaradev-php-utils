2bbae492a9afdf2e8cab93321847d30c58e371d0
bienvenu <bienvenu.randrianarison@pulse.mg>
2024-06-03 14:25:31 +0300

    Implement select features

--- src/Console/Console.php
+++ src/Console/Console.php
@@ -6,48 +6,66 @@ use SikaradevPhpUtils\Collections\Collection;
 
 final class Console
 {
-    public function __construct()
-    {
-    }
+	public function __construct() {}
 
-    public static function last(): Collection
-    {
-        $firstPattern = "/(\w+)\s\w+\s+(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) - (\w+)\s+\(\d{1,2}:\d{2}\)/";
+	public static function last(): Collection
+	{
 
-        $command = shell_exec('last $(whoami) -w --time-format iso -R | grep tty2');
+		$command = shell_exec('last $(whoami) -w --time-format iso -R | grep tty2');
 
-        $content = preg_replace('/\n/', '|', $command);
-        $items = explode('|', $content);
-        return Collection::of($items)
-            ->filter(fn(int $index, string $value) => $value != '')
-            ->map(fn(string $item) => preg_replace('/\s{2,}/', ' ', $item))
-            ->map(function ($item) {
-                if (preg_match(
-                    "/(?<username>\w+)\s\w+\s+(?<loginTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) - (?<logoutEvent>\w+)\s+\((?<sessionDuration>\d{2}:\d{2})\)/",
-                    $item,
-                    $match
-                )) {
-                    return [
-                        "username" => $match['username'],
-                        "loginTime" => $match['loginTime'],
-                        "logoutEvent" => $match['logoutEvent'],
-                        "sessionDuration" => $match['sessionDuration'],
-                    ];
-                }
-
-                if (preg_match(
-                    "/(?<username>\w+)\s\w+\s+(?<loginTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) - (?<logoutEvent>\w+)\s+\((?<sessionDuration>\d+\+\d{2}:\d{2})\)/",
-                    $item,
-                    $match
-                )) {
-                    return [
-                        "username" => $match['username'],
-                        "loginTime" => $match['loginTime'],
-                        "logoutEvent" => $match['logoutEvent'],
-                        "sessionDuration" => $match['sessionDuration'],
-                    ];
-                }
-                return $item;
-            });
-    }
+		$content = preg_replace('/\n/', '|', $command);
+		$items   = explode('|', $content);
+		return Collection::of($items)
+			->filter(fn(int $index, string $value) => $value != '')
+			->map(fn(string $item) => preg_replace('/\s{2,}/', ' ', $item))
+			->map(function ($item)
+			{
+				return match (true) {
+					preg_match(
+						"/(?<username>\w+)\s\w+\s+(?<loginTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) - (?<logoutEvent>\w+)\s+\((?<sessionDuration>\d{2}:\d{2})\)/",
+						$item, $match
+					)       => Collection::of($match)->select('username', 'loginTime', 'logoutEvent', 'sessionDuration')
+						->get(),
+					/*[
+						"username"        => $match['username'],
+						"loginTime"       => $match['loginTime'],
+						"logoutEvent"     => $match['logoutEvent'],
+						"sessionDuration" => $match['sessionDuration'],
+					],*/
+					preg_match(
+						"/(?<username>\w+)\s\w+\s+(?<loginTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) - (?<logoutEvent>\w+)\s+\((?<sessionDuration>\d+\+\d{2}:\d{2})\)/",
+						$item, $match
+					)
+					        => Collection::of($match)->select('username', 'loginTime', 'logoutEvent', 'sessionDuration')
+						->get(),
+					/*[
+						"username"        => $match['username'],
+						"loginTime"       => $match['loginTime'],
+						"logoutEvent"     => $match['logoutEvent'],
+						"sessionDuration" => $match['sessionDuration'],
+					],*/
+					preg_match(
+						"/(?<username>\w+)\s\w+\s+(?<loginTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) - (?<logoutTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) \((?<sessionDuration>\d{2}:\d{2})\)/",
+						$item, $match
+					)
+					        => Collection::of($match)->select('username', 'loginTime', 'sessionDuration')->get(),
+					/*	[
+							"username"        => $match['username'],
+							"loginTime"       => $match['loginTime'],
+							"sessionDuration" => $match['sessionDuration'],
+						],*/
+					preg_match(
+						"/(?<username>\w+)\s\w+\s+(?<loginTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) (?<logoutEvent>[a-z\s]+)/",
+						$item, $match
+					)
+					        => Collection::of($match)->select('username', 'loginTime', 'logoutEvent')->get(),
+					/*	[
+							"username"    => $match['username'],
+							"loginTime"   => $match['loginTime'],
+							"logoutEvent" => $match['logoutEvent']
+						],*/
+					default => $item
+				};
+			});
+	}
 }
