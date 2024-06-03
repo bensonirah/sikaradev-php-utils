40ca1b13cc85b20cc85571977bf7115189a12690
bensonirah <bensonirah@gmail.com>
2024-06-03 22:12:06 +0300

    Implemet select features

--- src/Collections/Collection.php
+++ src/Collections/Collection.php
@@ -1,114 +1,128 @@
 <?php
+
 declare(strict_types=1);
 
 namespace SikaradevPhpUtils\Collections;
 
 class Collection
 {
-	private array $list;
-	private int   $lenght;
+    private array $list;
+    private int $lenght;
 
-	/**
-	 * @param array $list
-	 */
-	public function __construct(array $list)
-	{
-		$this->list   = $list;
-		$this->lenght = count($list);
-	}
+    /**
+     * @param array $list
+     */
+    public function __construct(array $list)
+    {
+        $this->list = $list;
+        $this->lenght = count($list);
+    }
 
-	public static function of(array $list): self
-	{
-		return new static($list);
-	}
+    public static function of(array $list): self
+    {
+        return new static($list);
+    }
 
-	public function filter(callable $predicate): self
-	{
-		$filteredItems = [];
-		foreach ($this->list as $key => $value) {
-			if ($predicate($key, $value)) {
-				$filteredItems[] = $value;
-			}
-		}
-		$this->list = $filteredItems;
+    public function filter(callable $predicate): self
+    {
+        $filteredItems = [];
+        foreach ($this->list as $key => $value) {
+            if ($predicate($key, $value)) {
+                $filteredItems[] = $value;
+            }
+        }
+        $this->list = $filteredItems;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function map(callable $fn): self
-	{
-		$this->list = array_map($fn, $this->list);
-		return $this;
-	}
+    public function map(callable $fn): self
+    {
+        $this->list = array_map($fn, $this->list);
+        return $this;
+    }
 
-	/**
-	 * @param callable   $fn
-	 * @param mixed|null $initial
-	 *
-	 * @return $this
-	 */
-	public function reduce(callable $fn, mixed $initial = null): self
-	{
-		$this->list = array_reduce($this->list, $fn, $initial);
-		return $this;
-	}
+    /**
+     * @param callable $fn
+     * @param mixed|null $initial
+     *
+     * @return $this
+     */
+    public function reduce(callable $fn, mixed $initial = null): self
+    {
+        $this->list = array_reduce($this->list, $fn, $initial);
+        return $this;
+    }
 
-	public function get(): array
-	{
-		return $this->list;
-	}
+    public function get(): array
+    {
+        return $this->list;
+    }
 
-	public function count(): int
-	{
-		return count($this->list);
-	}
+    public function count(): int
+    {
+        return count($this->list);
+    }
 
-	/**
-	 * @param int $offset
-	 *
-	 * @return $this
-	 */
-	public function skip(int $offset): self
-	{
-		$this->list = array_slice($this->list, $offset);
-		return $this;
-	}
+    /**
+     * @param int $offset
+     *
+     * @return $this
+     */
+    public function skip(int $offset): self
+    {
+        $this->list = array_slice($this->list, $offset);
+        return $this;
+    }
 
-	public function take(int $length): self
-	{
-		$this->list = array_slice($this->list, 0, $length);
-		return $this;
-	}
+    public function take(int $length): self
+    {
+        $this->list = array_slice($this->list, 0, $length);
+        return $this;
+    }
 
-	public function asGenerator(): \Generator
-	{
-		foreach ($this->list as $item) {
-			yield $item;
-		}
-	}
+    public function asGenerator(): \Generator
+    {
+        foreach ($this->list as $item) {
+            yield $item;
+        }
+    }
 
-	/**
-	 * @return array
-	 */
-	public function draw(): array
-	{
-		$index = mt_rand(0, $this->lenght - 1);
-		return ['index' => $index, 'item' => $this->list[$index]];
-	}
+    /**
+     * @return array
+     */
+    public function draw(): array
+    {
+        $index = mt_rand(0, $this->lenght - 1);
+        return ['index' => $index, 'item' => $this->list[$index]];
+    }
 
-	public function select(): static
-	{
-		$selectedItems = [];
-		foreach (func_get_args() as $column) {
-			$selectedItems[$column] = $this->list[$column];
-		}
-		$this->list = $selectedItems;
-		return $this;
-	}
+    public function select(): static
+    {
+        $selectedColumns = func_get_args();
+        if (typeof(array_keys($this->list)[0]) == 'integer' && typeof(array_keys($this->list[0])[0]) == 'string') {
+            $this->list = array_map(function (array $row) use ($selectedColumns) {
+                $tmp = [];
+                foreach ($selectedColumns as $selectedColumn) {
+                    $tmp[$selectedColumn] = $row[$selectedColumn];
+                }
+                return $tmp;
+            }, $this->list);
+        }
 
-	public function then(\Closure $func): void
-	{
-		$func($this->list);
-	}
+        if (typeof(array_keys($this->list)[0]) == 'string') {
+            $tmp = [];
+            foreach ($selectedColumns as $selectedColumn) {
+                $tmp[$selectedColumn] = $this->list[$selectedColumn];
+            }
+            $this->list = $tmp;
+        }
+        return $this;
+    }
+
+    public function then(\Closure $func): void
+    {
+        $func($this->list);
+    }
 
 }
--- src/Console/Console.php
+++ src/Console/Console.php
@@ -6,66 +6,57 @@ use SikaradevPhpUtils\Collections\Collection;
 
 final class Console
 {
-	public function __construct() {}
+    public function __construct()
+    {
+    }
 
-	public static function last(): Collection
-	{
+    public static function table(array $elements)
+    {
+        dump($elements);
+    }
 
-		$command = shell_exec('last $(whoami) -w --time-format iso -R | grep tty2');
+    public static function last(): Collection
+    {
+        $command = shell_exec('last $(whoami) -w --time-format iso -R | grep tty2');
 
-		$content = preg_replace('/\n/', '|', $command);
-		$items   = explode('|', $content);
-		return Collection::of($items)
-			->filter(fn(int $index, string $value) => $value != '')
-			->map(fn(string $item) => preg_replace('/\s{2,}/', ' ', $item))
-			->map(function ($item)
-			{
-				return match (true) {
-					preg_match(
-						"/(?<username>\w+)\s\w+\s+(?<loginTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) - (?<logoutEvent>\w+)\s+\((?<sessionDuration>\d{2}:\d{2})\)/",
-						$item, $match
-					)       => Collection::of($match)->select('username', 'loginTime', 'logoutEvent', 'sessionDuration')
-						->get(),
-					/*[
-						"username"        => $match['username'],
-						"loginTime"       => $match['loginTime'],
-						"logoutEvent"     => $match['logoutEvent'],
-						"sessionDuration" => $match['sessionDuration'],
-					],*/
-					preg_match(
-						"/(?<username>\w+)\s\w+\s+(?<loginTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) - (?<logoutEvent>\w+)\s+\((?<sessionDuration>\d+\+\d{2}:\d{2})\)/",
-						$item, $match
-					)
-					        => Collection::of($match)->select('username', 'loginTime', 'logoutEvent', 'sessionDuration')
-						->get(),
-					/*[
-						"username"        => $match['username'],
-						"loginTime"       => $match['loginTime'],
-						"logoutEvent"     => $match['logoutEvent'],
-						"sessionDuration" => $match['sessionDuration'],
-					],*/
-					preg_match(
-						"/(?<username>\w+)\s\w+\s+(?<loginTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) - (?<logoutTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) \((?<sessionDuration>\d{2}:\d{2})\)/",
-						$item, $match
-					)
-					        => Collection::of($match)->select('username', 'loginTime', 'sessionDuration')->get(),
-					/*	[
-							"username"        => $match['username'],
-							"loginTime"       => $match['loginTime'],
-							"sessionDuration" => $match['sessionDuration'],
-						],*/
-					preg_match(
-						"/(?<username>\w+)\s\w+\s+(?<loginTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) (?<logoutEvent>[a-z\s]+)/",
-						$item, $match
-					)
-					        => Collection::of($match)->select('username', 'loginTime', 'logoutEvent')->get(),
-					/*	[
-							"username"    => $match['username'],
-							"loginTime"   => $match['loginTime'],
-							"logoutEvent" => $match['logoutEvent']
-						],*/
-					default => $item
-				};
-			});
-	}
+        $content = preg_replace('/\n/', '|', $command);
+        $items = explode('|', $content);
+        return Collection::of($items)
+            ->filter(fn(int $index, string $value) => $value != '')
+            ->map(fn(string $item) => preg_replace('/\s{2,}/', ' ', $item))
+            ->map(function ($item) {
+                return match (1) {
+                    preg_match(
+                        "/(?<username>\w+)\s\w+\s+(?<loginTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) - (?<logoutEvent>\w+)\s+\((?<sessionDuration>\d{2}:\d{2})\)/",
+                        $item,
+                        $match
+                    ) => Collection::of($match)->select('username', 'loginTime', 'logoutEvent', 'sessionDuration')
+                        ->get(),
+                    preg_match(
+                        "/(?<username>\w+)\s\w+\s+(?<loginTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) - (?<logoutEvent>\w+)\s+\((?<sessionDuration>\d+\+\d{2}:\d{2})\)/",
+                        $item,
+                        $match
+                    )
+                    => Collection::of($match)->select('username', 'loginTime', 'logoutEvent', 'sessionDuration')
+                        ->get(),
+                    preg_match(
+                        "/(?<username>\w+)\s\w+\s+(?<loginTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) - (?<logoutTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) \((?<sessionDuration>\d{2}:\d{2})\)/",
+                        $item,
+                        $match
+                    ) => Collection::of($match)->select('username', 'loginTime', 'sessionDuration')->get(),
+                    preg_match(
+                        "/(?<username>\w+)\s\w+\s+(?<loginTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) (?<logoutEvent>[a-z\s]+)/",
+                        $item,
+                        $match
+                    ) => Collection::of($match)->select('username', 'loginTime', 'logoutEvent')->get(),
+                    preg_match(
+                        "/(?<username>\w+)\s\w+\s+(?<loginTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) - (?<logoutTime>\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}) \((?<sessionDuration>\d+\+\d{2}:\d{2})\)/",
+                        $item,
+                        $match
+                    ) => Collection::of($match)->select('username', 'loginTime', 'logoutTime', 'sessionDuration')->get(
+                    ),
+                    default => $item
+                };
+            });
+    }
 }
