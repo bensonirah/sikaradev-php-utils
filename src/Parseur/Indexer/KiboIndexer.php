629230947c3ee4bb9b60e23c8e65fe576f1d2db8
bensonirah <bensonirah@gmail.com>
2024-06-28 13:09:23 +0300

    Update metadata indexer

--- src/Parseur/Indexer/KiboIndexer.php
+++ src/Parseur/Indexer/KiboIndexer.php
@@ -2,29 +2,26 @@
 
 namespace SikaradevPhpUtils\Parseur\Indexer;
 
-use SikaradevPhpUtils\Parseur\Website\UrlProcessor;
 use Symfony\Component\DomCrawler\Crawler;
 
 final class KiboIndexer implements IndexerInterface
 {
 
-    /**
-     * @param Crawler $crawler
-     * @return array
-     */
-    public function visualize(Crawler $crawler): array
-    {
-        return $crawler->filter('.products.row > article')
-            ->each(function (Crawler $node, $i) {
-                $permalink = $node->filter('.product-image-container>a:nth-child(1)')->attr('href');
-                return (new ProductResponse(
-                    $node->attr('data-id-product'),
-                    $node->filter('.product-title a')->text(),
-                    $permalink,
-                    $node->filter('img')->attr('src'),
-                    $node->filter('.price')->text()
-                ))->withMetaData(UrlProcessor::metaTag($permalink));
-            }
-            );
-    }
+	/**
+	 * @param Crawler $crawler
+	 *
+	 * @return array
+	 */
+	public function visualize(Crawler $crawler): array
+	{
+		return $crawler->filter('.products.row > article')
+			->each(fn(Crawler $node, $i) => new ProductResponse(
+				$node->attr('data-id-product'),
+				$node->filter('.product-title a')->text(),
+				$node->filter('.product-image-container>a:nth-child(1)')->attr('href'),
+				$node->filter('img')->attr('src'),
+				$node->filter('.price')->text()
+			)
+			);
+	}
 }
